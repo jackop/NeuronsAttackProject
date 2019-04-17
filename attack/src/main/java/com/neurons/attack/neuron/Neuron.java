3b679c307aa5abfd09e6f244ff23fb0fff2271c7
Jacek Koprowski <jacekkoprowski@MacBook-Pro-Jacek.local>
2019-04-17 22:59:44 +0200

    Refactoring

--- attack/src/main/java/com/neurons/attack/neuron/Neuron.java
+++ attack/src/main/java/com/neurons/attack/neuron/Neuron.java
@@ -1,23 +1,29 @@
 package com.neurons.attack.neuron;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
-import java.util.stream.IntStream;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
 
 public class Neuron {
 	
 	protected ArrayList<Double> inputs;
 	protected ArrayList<Double> weights;
+	private final static Integer RANDOM_FROM = -1;
+	private final static Integer RANDOM_TO = 1;
 	
 	public Neuron(int numberOfInputs) {
-		IntStream.range(0, numberOfInputs)
-		.forEach(a -> System.out.println(randomDoubleValue(-1, 1)));
+		randomDoubleValue(numberOfInputs).forEach(action -> System.out.println(action));
 	}
 	
-	private Double randomDoubleValue(double start, double end) {
-		Random random = new Random();
-		double result = start + (random.nextDouble() * (end - start));
-		
-		return result;
+	private List<Double> randomDoubleValue(int numberOfInputs) {
+		return DoubleStream.iterate(0, n -> numberOfInputs).limit(numberOfInputs)
+				.map(n -> generateRandom(RANDOM_FROM, RANDOM_TO))
+	            .boxed().collect(Collectors.toList());
+	}
+	
+	private Double generateRandom(int start, int end) {
+		return start + (new Random().nextDouble() * (end - start));
 	}
 }
